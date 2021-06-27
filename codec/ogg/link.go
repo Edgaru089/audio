249fa0c47626f2bc41209a8dabec7bfbfb7e327c
Edgaru089 <sjs333@outlook.com>
2021-06-27 15:52:19 +0800

    attach openal32.dll & fix linking ogg on Windows (still broken though)

--- codec/ogg/link.go
+++ codec/ogg/link.go
@@ -3,6 +3,6 @@ package ogg
 // #cgo linux darwin LDFLAGS: -lvorbisfile -lvorbisenc -lvorbis
 //
 // #cgo windows        CFLAGS: -I./extlib/include
-// #cgo windows,386   LDFLAGS: -L./extlib/lib-mingw-32 -lvorbisfile -lvorbisenc -lvorbis
-// #cgo windows,amd64 LDFLAGS: -L./extlib/lib-mingw-64 -lvorbisfile -lvorbisenc -lvorbis
+// #cgo windows,386   LDFLAGS: -L./extlib/lib-mingw-32 -lvorbisfile -lvorbisenc -lvorbis -logg
+// #cgo windows,amd64 LDFLAGS: -L./extlib/lib-mingw-64 -lvorbisfile -lvorbisenc -lvorbis -logg
 import "C"
